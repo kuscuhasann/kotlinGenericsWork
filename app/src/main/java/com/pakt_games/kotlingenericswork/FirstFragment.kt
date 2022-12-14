ba2af729f1fa483b0b2db1860ba42d3bffa750ff
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2022-12-15 02:13:53 +0300

    implemented generics

---
+++ app/src/main/java/com/pakt_games/kotlingenericswork/FirstFragment.kt
@@ -0,0 +1,73 @@
+package com.pakt_games.kotlingenericswork
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.pakt_games.kotlingenericswork.R
+import com.pakt_games.kotlingenericswork.databinding.FragmentFirstBinding
+import com.pakt_games.kotlingenericswork.di.firstFragmentModule
+import com.pakt_games.kotlingenericswork.model.City
+import com.pakt_games.kotlingenericswork.viewmodel.FirstFragmentViewModel
+import kotlinx.coroutines.flow.collect
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    private var firstFragmentViewModel = FirstFragmentViewModel()
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dependecyInversion()
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    private fun dependecyInversion() {
+        startKoin {
+            androidContext(requireActivity())
+            modules(firstFragmentModule)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        firstFragmentViewModel.cityCode = City(16)
+        firstFragmentViewModel.cityName = City("Bursa")
+        clickActions()
+    }
+
+    private fun clickActions() {
+        lifecycleScope.launchWhenStarted {
+            firstFragmentViewModel.cityCodeClickAction.collect() {
+                Toast.makeText(requireContext(), "City Code: $firstFragmentViewModel.cityName", Toast.LENGTH_SHORT)
+            }
+            firstFragmentViewModel.cityNameClickAction.collect() {
+                Toast.makeText(requireContext(), "City Name: $firstFragmentViewModel.cityCode", Toast.LENGTH_SHORT)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
